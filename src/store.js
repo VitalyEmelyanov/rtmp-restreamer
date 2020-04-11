c5aeb4e4c6e6152c49904f4623dce9e9014844d6
Vitaly Emelyanov <emelyanov.vk@gmail.com>
2020-04-11 15:18:12 +0300

    Added Facebook support

--- src/store.js
+++ src/store.js
@@ -1,3 +1,7 @@
+/* global __static */
+
+import fs from 'fs'
+import path from 'path'
 import Vue from 'vue';
 import Vuex from 'vuex';
 import Docker from 'dockerode';
@@ -6,6 +10,7 @@ import getPort from 'get-port';
 Vue.use(Vuex);
 
 export const IMAGE_ID = 'jasonrivers/nginx-rtmp:latest'
+export const STUNNEL_IMAGE_ID = 'stunnel-rmtp:latest'
 
 export const store = new Vuex.Store({
   plugins: [
@@ -50,12 +55,17 @@ export const store = new Vuex.Store({
       commit('setError', err)
       commit('setLoading', false)
     },
-    async init({dispatch, commit}) {
+    async init({dispatch, commit, state}) {
       commit('setLoading', true)
       dispatch('log', 'Initializing')
       await dispatch('connectDocker')
-      await dispatch('pullImage')
+      if (state.error) return
+      await dispatch('pullRtmpImage')
+      if (state.error) return
+      await dispatch('buildStunnelImage')
+      if (state.error) return
       await dispatch('killExistingContainers')
+      if (state.error) return
       dispatch('log', 'Ready')
       commit('setLoading', false)
     },
@@ -76,27 +86,78 @@ export const store = new Vuex.Store({
       dispatch('log', `Got RTMP port ${rtmpPort}`)
       dispatch('log', `Got HTTP port ${httpPort}`)
 
-      dispatch('log', 'Creating container')
-
-      const urls = state.streamingDestinations.map(d => {
-        const url = String(d.url).trim().replace(/\/+$/, '')
-        const key = String(d.key).trim()
-        return `${url}/${key}`
-      })
+      dispatch('log', 'Creating containers')
 
       try {
-        const container = await state.docker.createContainer({
+        const stunnelContainer = await state.docker.createContainer({
+          Image: STUNNEL_IMAGE_ID,
+          Hostname: 'stunnel',
+          Tty: true,
+          "ExposedPorts": {
+            "19350/tcp": { }
+          },
+          HostConfig: {
+            PortBindings: {
+              '19350/tcp': [{HostPort: '19350'}],
+            },
+          }
+        })
+
+        dispatch('log', 'Starting Stunnel container')
+        await stunnelContainer.start()
+
+        stunnelContainer.attach({stream: true, stdout: true, stderr: true}, function (err, stream) {
+          stream.pipe(process.stdout);
+        });
+        
+        const stunnelInfo = await stunnelContainer.inspect()
+        const stunnelHost = String(stunnelInfo.Name).replace('/', '')
+
+        const urls = state.streamingDestinations.map(d => {
+          const url = String(d.url).trim().replace(/\/+$/, '')
+          const key = String(d.key).trim()
+          if (url.includes('facebook')) {
+            return `rtmp://${stunnelHost}:19350/rtmp/${key}`
+          }
+          return `${url}/${key}`
+        })
+
+        const nginxTemplatePath = path.join(__static, '/rtmp/nginx.conf.template')
+        const nginxConfigPath = path.join(__static, '/rtmp/nginx.conf')
+
+        let template = fs.readFileSync(nginxTemplatePath, 'utf8')
+
+        template = template.replace('PUSH_URLS', urls.map(url => `push ${url};`).join('\n'))
+
+        fs.writeFileSync(nginxConfigPath, template, 'utf8')
+        
+        const rtpmContainer = await state.docker.createContainer({
           Image: IMAGE_ID,
-          Env: [`RTMP_PUSH_URLS=${urls.join(',')}`],
           Tty: true,
           PortBindings: {
             '1935/tcp': [{HostPort: String(rtmpPort)}],
             '8080/tcp': [{HostPort: String(httpPort)}],
           },
+          HostConfig: {
+            Binds: [
+              `${nginxConfigPath}:/opt/nginx/conf/nginx.conf`
+            ],
+            PortBindings: {
+              '1935/tcp': [{HostPort: String(rtmpPort)}],
+              '8080/tcp': [{HostPort: String(httpPort)}],
+            },
+            Links: [
+              `${stunnelHost}:stunnel`
+            ],
+          }
         })
 
-        dispatch('log', 'Starting container')
-        await container.start()
+        dispatch('log', 'Starting RTMP container')
+        await rtpmContainer.start()
+
+        rtpmContainer.attach({stream: true, stdout: true, stderr: true}, function (err, stream) {
+          stream.pipe(process.stdout);
+        });
 
         commit('setRtmpPort', rtmpPort)
       } catch (e) {
@@ -126,7 +187,7 @@ export const store = new Vuex.Store({
       try {
         const containers = await state.docker.listContainers({
           all: true,
-          filters: {ancestor: [IMAGE_ID]}
+          filters: {ancestor: [IMAGE_ID, STUNNEL_IMAGE_ID]}
         })
         for (const container of containers) {
           await state.docker.getContainer(container.Id).remove({force: true})
@@ -136,16 +197,40 @@ export const store = new Vuex.Store({
         dispatch('error', e)
       }
     },
-    async pullImage({dispatch, state}) {
-      dispatch('log', 'Checking image')
+    async pullRtmpImage({dispatch, state}) {
+      dispatch('log', 'Checking RTMP')
       try {
         const images = await state.docker.listImages()
         const exists = images.find(i => i.RepoTags.includes(IMAGE_ID))
         if (exists) {
-          dispatch('log', 'Image already exists')
+          dispatch('log', 'RTMP already exists')
         } else {
-          dispatch('log', 'Pulling image, please wait')
-          await state.docker.pull(IMAGE_ID)
+          dispatch('log', 'Pulling RTMP, please wait')
+          const stream = await state.docker.pull(IMAGE_ID)
+          await new Promise((resolve, reject) => {
+            state.docker.modem.followProgress(stream, (err, res) => err ? reject(err) : resolve(res));
+          })
+        }
+      } catch (e) {
+        dispatch('error', e)
+      }
+    },
+    async buildStunnelImage({dispatch, state}) {
+      dispatch('log', 'Checking Stunnel')
+      try {
+        const images = await state.docker.listImages()
+        const exists = images.find(i => i.RepoTags.includes(STUNNEL_IMAGE_ID))
+        if (exists) {
+          dispatch('log', 'Stunnel already exists')
+        } else {
+          dispatch('log', 'Building Stunnel, please wait')
+          const stunnelPath = path.join(__static, '/stunnel')
+          const stream = await state.docker.buildImage({
+            context: stunnelPath,
+          }, {t: STUNNEL_IMAGE_ID});
+          await new Promise((resolve, reject) => {
+            state.docker.modem.followProgress(stream, (err, res) => err ? reject(err) : resolve(res));
+          })
         }
       } catch (e) {
         dispatch('error', e)
